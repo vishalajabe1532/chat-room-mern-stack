3c9a818a65ad3b07edbaa1db7e398657056c1693
vishal <vishala.ajabe1532@gmail.com>
2024-09-06 01:10:14 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 
 
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -18,6 +19,8 @@ import { app, server } from './socket/socket.js';
 const PORT = process.env.PORT || 5000
 
 
+const __dirname = path.resolve()
+
 
 dotenv.config();
 
@@ -32,11 +35,15 @@ app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
-// app.get("/",(req,res)=>{
-//     //root route
-//     res.send("server is ready");
-// })
+
+
+
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 
 
