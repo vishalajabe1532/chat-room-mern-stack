76aced52b47bc87f2a0c430a9cff617d7e90a76d
vishal <vishala.ajabe1532@gmail.com>
2024-09-06 14:48:14 +0530

    fixtures 2

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
@@ -12,6 +13,7 @@ import userRoutes from './routes/user.routes.js'
 
 import connectToMongoDB from './db/connectTomongoDB.js';
 import { app, server } from './socket/socket.js';
+
 
 
 
@@ -32,6 +34,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
